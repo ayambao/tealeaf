6d8ba5cc9dc7f726c1ab844dffc5a21434c29cfa
ayambao <allanyambao@gmail.com>
2015-09-18 09:54:50 +0200

    age_checker_revise

---
+++ age_check.rb
@@ -0,0 +1,13 @@
+puts "Enter Age"
+age = gets.chomp.to_i
+
+if (age >= 5) && (age <= 6)
+  put "You'r in kindergarten"
+elsif (age >= 7) && (age <= 13)
+  puts "You'r in midle school"
+  puts "O yeah!"
+else
+  puts "Stay HOme"
+end
+
+
